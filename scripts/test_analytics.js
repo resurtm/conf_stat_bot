c5b6410434bf9bcb75af6ff8be722affbe5128ce
resurtm <resurtm@gmail.com>
2017-11-17 21:28:23 +0600

    analytics wip

--- scripts/test_analytics.js
+++ scripts/test_analytics.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env node
 
-const analytics = require('../src/analytics');
 const _ = require('lodash');
+const analytics = require('../src/analytics');
+const db = require('../src/db');
 
 const chatID = process.argv[2];
 
 console.log('topPostersForLast24Hours:');
 analytics.topPostersForLast24Hours(chatID)
-  .then(res => {
-    _.forEach(res.aggregations.users.buckets, value => {
-      console.log('\tkey = ' + value.key + ', count = ' + value.doc_count);
-    });
-  })
+  .then(res => console.log(res))
+  .then(() => db.knex.destroy())
   .catch(err => console.log(err));
--- src/analytics.js
+++ src/analytics.js
@@ -1,13 +1,14 @@
+const _ = require('lodash');
 const elastic = require('../src/elastic');
+const db = require('../src/db');
 
-function topPostersForLast24Hours(chatID) {
+function topPostersForLast24HoursInternal(chatID) {
   const ts = Math.round(+new Date() / 1000);
   return elastic.client.search({
     index: 'conf-stat-bot-user-message',
     body: {
       from: 0,
       size: 0,
-
       query: {
         bool: {
           must: [
@@ -16,7 +17,6 @@ function topPostersForLast24Hours(chatID) {
                 tg_chat_id: chatID.toString(),
               },
             },
-
             {
               range: {
                 timestamp: {
@@ -28,7 +28,6 @@ function topPostersForLast24Hours(chatID) {
           ],
         },
       },
-
       aggregations: {
         users: {
           terms: {
@@ -41,6 +40,31 @@ function topPostersForLast24Hours(chatID) {
   });
 }
 
+function topPostersForLast24Hours(chatID) {
+  const docCounts = {};
+  return topPostersForLast24HoursInternal(chatID)
+    .then(res => {
+      const promises = [];
+      _.forEach(res.aggregations.users.buckets, value => {
+        docCounts[value.key] = value.doc_count;
+        promises.push(db.User.where('tg_user_id', value.key).fetch());
+      });
+      return Promise.all(promises);
+    })
+    .then(res => {
+      const result = {};
+      _.forEach(res, value => {
+        const attrs = value.attributes;
+        result[attrs.tg_user_id] = {
+          messageCount: docCounts[attrs.tg_user_id],
+          displayName: attrs.user_name.length > 0 ? attrs.user_name : attrs.first_name,
+          hasUserName: attrs.user_name.length > 0,
+        };
+      });
+      return result;
+    });
+}
+
 module.exports = {
-  topPostersForLast24Hours
+  topPostersForLast24Hours,
 };
